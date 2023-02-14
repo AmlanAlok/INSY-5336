8b18b0dab3e75a30d28859bd76cd95af63d84264
Amlan Alok <amlanalok@gmail.com>
2023-02-14 10:08:39 -0600

    week 2 prep added

---
+++ Lecture/Week-2/week_2.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+def floor_func():
+    x = 5 // 2      # 2
+    print(x)
+    y = -5 // 2     # -3
+    print(y)
+
+
+def reading_input():
+    x = int(input('Enter: '))
+    print(type(x))
+
+
+
+
+
+if __name__ == '__main__':
+
+    if 'marya' > 'mary':
+        print(True)
+    else:
+        print(False)
