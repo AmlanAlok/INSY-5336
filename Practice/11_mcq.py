d2bc097dc9a7664f7621469700ffbc3e92d2e291
Amlan Alok <amlanalok@gmail.com>
2023-03-30 19:02:41 -0500

    mcq Exam-2 question

--- Practice/11_mcq.py
+++ Practice/11_mcq.py
@@ -4,8 +4,16 @@ import unittest
 class MyTestCase(unittest.TestCase):
 
     def test_1(self):
+        lst = [3, 6, 9]
+        dc = {}
+        print(type(dc))
 
-        self.assertEqual(True, False)
+        for i in lst:
+            dc = i*i
+            print(type(dc))
+
+        print(dc)
+        # self.assertEqual(True, False)
 
 
 if __name__ == '__main__':
