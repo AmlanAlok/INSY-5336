02021f78783d14ad942d519771c3315c58a2500c
Amlan Alok <amlanalok@gmail.com>
2023-04-21 21:46:31 -0500

    nested class

---
+++ Lecture/Week-11/nested_class.py
@@ -0,0 +1,27 @@
+import unittest
+
+"""Question kj"""
+class Vehicle:
+
+    def __init__(self, x):
+        self.x = x
+
+    class Car:
+        def __init__(self, y):
+            self.y = y
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_something(self):
+
+        z = Vehicle(5)
+        q = z.Car(2)
+        # p = Car(9)
+
+
+        # self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
