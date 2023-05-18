7f83ea37c3fe49ae8fe1476f370f5d45b7b9c656
Amlan Alok <amlanalok@gmail.com>
2023-05-17 23:19:13 -0500

    re lib

--- Lecture/Week-12/1_tue_re.py
+++ Lecture/Week-12/1_tue_re.py
@@ -1,21 +1,38 @@
 import re
 
-text = 'I love UTD. Because UTD is awesome. This is 2023'
-z1 = re.sub('UTD', 'UTA', text)
-# print(z1)
-assert z1 == 'I love UTA. Because UTA is awesome. This is 2023'
-
-z2 = re.sub('[a-z]', '', text)
-print(z2)
-assert z2 == 'I **** UTD. B****** UTD ** *******. T*** ** 2023'
-
-t2 = '2023 is a great year'
-z3 = re.sub('\d', '', t2)
-assert z3 == ' is a great year'
-# print(z3)
-
+# text = 'I love UTD. Because UTD is awesome. This is 2023'
+# z1 = re.sub('UTD', 'UTA', text)
+# # print(z1)
+# assert z1 == 'I love UTA. Because UTA is awesome. This is 2023'
+#
+# z2 = re.sub('[a-z]', '', text)
+# print(z2)
+# assert z2 == 'I **** UTD. B****** UTD ** *******. T*** ** 2023'
+#
+# t2 = '2023 is a great year'
+# z3 = re.sub('\d', '', t2)
+# assert z3 == ' is a great year'
 
 
+# t9 = '#2022 is awesome'
+# res = re.sub(r'\s+', 'X', t9)
+# print(res)
+# # print(z3)
+# i=0
+# while i<5:
+#     i+=1
+#     print(i)
+# else:
+#     print('p')
+
+
+# for i in range(5):
+#     print(i)
+# else:
+#     print('p')
+
+
+# print((1,2,3) +(4,5))
 
 
 
